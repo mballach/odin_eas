e5a2f3dd4d2747ccf3ba636fa9cd932ecb3d347a
mballach <muggins111@live.ca>
2024-11-04 15:54:49 -0800

    Add README Update styling of EAS Add opacity to mouseover logic

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # odin_eas
+This is an etch-a-sketch project created for a lesson on the Odin Project. The etch-a-sketch uses Javascript to allow users to draw on a sketchpad of small squares.
--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const clrButton = document.querySelector('.clearbutton')
 clrButton.addEventListener("click",clearButton);
 
 function pixelActive(e){
-    e.target.style.backgroundColor="black";
+    const currentOpacity = +e.target.style.opacity
+    e.target.style.opacity = currentOpacity+0.1;
+    
 }
 
 
-function generatePad(x=100) {
+function generatePad(x=10) {
     for (let i=0;i<x;i++) {
         let newRow = document.createElement('div');
         newRow.setAttribute("class","row")
@@ -28,11 +30,11 @@ function generatePad(x=100) {
 generatePad()
 
 function clearButton(){
-    sketch.innerHTML="";
-    let newSize = prompt("Please enter your new size")
-    while (newSize>100){
-        newSize = prompt("Please enter a number less than 100")
+    let newSize = prompt("Please enter your new size, up to 100")
+    while (newSize>100||newSize<=0){
+        newSize = prompt("Please enter a positive number that is less than 100")
     }
+    sketch.innerHTML="";
     generatePad(newSize);
 
 }
